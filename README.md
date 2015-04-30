989311f2e8016c761f175af198456536ba3ab0ad
Eugene Lazarchuk <elazarchuk@alvioneurope.ru>
2015-04-30 14:33:30 +0300

    Added centering for cells

--- README.md
+++ README.md
@@ -6,17 +6,11 @@ A modified version of mstrehse's Honeycombs jQuery Plugin. It produces responsiv
 ![Demo Image](https://raw.githubusercontent.com/Tiotao/honeycombs/master/demo.png)
 
 ## Original
-- Author: mstrehse
-- Repo: https://github.com/mstrehse/honeycombs
-- Example: http://examples.rabbid.net/honeycombs/demo.html
+- Author: Tiotao
+- Repo: https://github.com/Tiotao/honeycombs.js/
 
 ## Modifications
-- No longer support hexagon with background image as the hexagon is now made by iconic font rather than CSS.
-
-## Improvements
-- Support IE8
-- Responsive for single column orientation (in mobile screens)
-- placehoder class provides more flexibility in arranging hexagons.
+- Added centering cells
 
 ## Usage
 To generate a honeycomb grid, use the following markup:
@@ -56,4 +50,3 @@ and add the following code at the end of the `<body>` tag
 });
 </script>
 ```
-
--- honeycombs/js/jquery.honeycombs.js
+++ honeycombs/js/jquery.honeycombs.js
@@ -60,7 +60,7 @@
                 combWidth = settings.combWidth;
                 combHeight = combWidth;
                 $(element).find('.comb').width(combWidth).height(combHeight);
-                $(element).find('.icon-hex-lg').css('font-size', combWidth)
+                $(element).find('.icon-hex-lg').css('font-size', combWidth);
             }
             
             /**
@@ -85,57 +85,151 @@
                 var offset = 0; // 1 is down
                 var left = 1; // pos left
                 var top = 0; // pos top
-                var cols = 0;
+//                var cols = 0;
 
                 var noOffset = function(offset){
-                    return offset
+                    return offset;
                 };
 
                 var withOffset = function(offset){
                     return (offset + 1) % 2;
-                }
+                };
 
                 var halfTop = function(top){
-                    return ( row * ( 0.5 * combHeight * Math.sqrt(3) + settings.margin) )
-                }
+                    return ( row * ( 0.5 * combHeight * Math.sqrt(3) + settings.margin) );
+                };
 
                 var fullTop = function(top){
-                    return ( row * (combHeight + settings.margin + combHeight * 0.1))
-                }
+                    return ( row * (combHeight + settings.margin + combHeight * 0.1));
+                };
+                
+                var mapArrayFromCenter = function(itemsAmount, arrayLen){
+                    var mapping = [];
+                    for(var i = 0;  i < arrayLen; i++){
+                        mapping[i] = false ;
+                    }
+                    
+                    if(arrayLen % 2 === 1){
+                        var centerIndex = Math.floor(arrayLen / 2);
+                        var currentIndex = 0;
+                        var deltaIndex = 1;
+                        var startIndex = 0;
+                        if(itemsAmount % 2 === 1){
+                            startIndex = 1;
+                            mapping[centerIndex] = true;
+                        }
+                        for(var i = startIndex;  i < itemsAmount; i++){
+                            if(itemsAmount % 2 === 1){
+                                currentIndex = i % 2 === 1 ? centerIndex + deltaIndex : centerIndex - deltaIndex++;
+                            } else {
+                                currentIndex = i % 2 === 0 ? centerIndex + deltaIndex : centerIndex - deltaIndex++;
+                            }
+                            mapping[currentIndex] = true ;
+                        }
+                        
+                    } else {
+                        var centerIndex = Math.floor(arrayLen / 2);
+                        for(var i = 0;  i < itemsAmount / 2; i++){
+                            mapping[centerIndex + i] = true ;
+                            mapping[centerIndex - 1 - i] = true ;
+                        }
+                    }
+                    
+                    return mapping;
+                };
 
                 function orderCombs(leftHandler, topHandler){
+                    var elements = $(element).find('.comb').filter(':not(.placeholder.hide)');
+                    
+                    // расчитать базовое количество элементов в первой строке
+                    var firstRowCount = calculateFirstRowCount(left, settings.margin, $(element).find('.comb').filter(':not(.placeholder.hide)'));
+                    
+                    // расчитываем матрицу положения элементов
+                    var positions = calculateMatrix(firstRowCount, elements.length);
 
-                    $(element).find('.comb').filter(':not(.placeholder.hide)').each(function(index){
-                        
-                        top = topHandler(top);
+                    // оттображаем элементы согласно матрице
+                    var elementIndex = 0;
+                    for(var i in positions){
+                        row = i;
+                        for(var j in positions[i]){
+                            top = topHandler(top);
+                            if(positions[i][j]){
+                                var currentElement = elements[elementIndex++];
+                                if(animate === true){
+                                    $(currentElement).stop(true, false);
+                                    $(currentElement).animate({'left': left, 'top': top});
+                                }else{
+                                    $(currentElement).css('left', left).css('top', top);
+                                }
+                            }
+                            left = left + ( combWidth + settings.margin );
 
-                        if(animate == true){
-                            $(this).stop(true, false);
-                            $(this).animate({'left': left, 'top': top});
-                        }else{
-                            $(this).css('left', left).css('top', top);
+                            if(left > maxLeft) {
+                                maxLeft = left;
+                            }
                         }
-                        
-                        left = left + ( combWidth + settings.margin );
-                        
-                        if(left > maxLeft) {
-                            maxLeft = left;
-                        }
-                       
-                        
-                        if(row == 0){
-                            cols = cols + 1;
-                        }
-                            
-                        if(left + combWidth > width){
-                            row = row + 1;
-                            offset = leftHandler(offset);
-                            left = offset / 2 * ( combWidth + settings.margin ) ;
-                        }
-
-                    });
+                        offset = leftHandler(offset);
+                        left = offset / 2 * ( combWidth + settings.margin ) ;
+                    }
                 }
 
+                var calculateFirstRowCount = function(initialLeftPosition, settingsMargin,elements){
+                    var columnCounter = 0;
+                    var currentLeft = initialLeftPosition;
+
+                    for(var i in elements){
+                        columnCounter++;
+                        currentLeft = currentLeft + ( combWidth + settingsMargin );
+                        if(currentLeft + combWidth > width){
+                             break;
+                        }
+                    }
+                    return columnCounter;
+                };
+                
+                var calculateMatrix = function(firstRowCount, elementsCount){
+                    var positions = [];
+                    var rowIndex = 0;
+                    var counter = 0;
+                    
+                    while(counter < elementsCount){
+                        var currentRowCount = rowIndex % 2 === 0 ? firstRowCount : firstRowCount -1;
+                        var nearRowCount = rowIndex % 2 === 0 ? firstRowCount - 1 : firstRowCount;
+                        var leftItems = elementsCount - counter;
+                        counter += currentRowCount;
+                        if(leftItems < currentRowCount){
+                            // Четная строка и на ней нечетное количество элементов необходимо разбить
+                            if( (currentRowCount) % 2 === 0 && (leftItems % 2 === 1) ){
+                                // если остался только 1 элемент, то пересобираем матрицу и уменьшаем количество элементов
+                                if(leftItems === 1){
+                                    positions = calculateMatrix(firstRowCount - 1, elementsCount);
+                                } else {
+                                    positions.push(mapArrayFromCenter(leftItems - 1, currentRowCount));
+                                    positions.push(mapArrayFromCenter(1, nearRowCount));
+                                }
+                            } else {
+                                positions.push(mapArrayFromCenter(leftItems, currentRowCount));
+                            }
+                            break;
+                        } else if(leftItems === currentRowCount){
+                            positions.push(addFilledRow(currentRowCount));
+                            break;
+                        } else {
+                            positions.push(addFilledRow(currentRowCount));
+                        }
+                        rowIndex++;
+                    }
+                    
+                    return positions;
+                };
+
+                var addFilledRow = function(count){
+                    var result = [];
+                    for(var i = 0; i < count; i++){
+                        result[i] = true;
+                    }
+                    return result;
+                };
 
                 if (newWidth < 1.5 * (combWidth + settings.margin)) {
                     $('.comb.placeholder').addClass('hide');
@@ -152,8 +246,8 @@
                 
                 $wrapper
                     .height(top + combHeight)
-                    .width(maxLeft - settings.margin)
-            }
+                    .width(maxLeft - settings.margin);
+            };
             
             $(window).resize(function(){
                 reorder(true);
@@ -167,6 +261,6 @@
             initialise(this);
         });
 
-    }
+    };
 
 }(jQuery));
